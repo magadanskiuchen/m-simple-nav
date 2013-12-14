c9cdfbea569dd564f4d3a8ef81eff2e49cf243c7
Georgi Popov <magadanski.uchen@gmail.com>
2013-12-15 01:57:02 +0200

    code refactoring and performance optimizations

--- js/functions.js
+++ js/functions.js
@@ -1,24 +1,94 @@
-var simpleNav;
+// define packages
+com = typeof(com) == 'undefined' ? {} : com;
+com.magadanski = typeof(com.magadanski) == 'undefined' ? {} : com.magadanski;
+com.magadanski.mSimpleNav = typeof(com.magadanski.mSimpleNav) == 'undefined' ? {} : com.magadanski.mSimpleNav;
 
-window.applicationCache.addEventListener('updateready', function (e) {
-	window.applicationCache.swapCache();
-}, false);
+// class instantiation (header)
+com.magadanski.mSimpleNav.GPS,
+com.magadanski.mSimpleNav.Compass,
+com.magadanski.mSimpleNav.MSimpleNav;
 
-function mSimpleNav() {
-	var currentLocation, destination,
-		destinationBearing, deviceBearing = 0, compassBearing = 0,
-		deviceTilt = 0, tiltProperty = 'beta', tiltQuantifier = 1,
-		currentLocationTimeout, deviceBearingTimeout,
-		intervalDelay = 100;
+//////////////////////////////////////
+// GPS Class Implementation //////////
+//////////////////////////////////////
+(function () {
+	// import class
+	var that;
+	var GPS = function (enableHighAccuracy) {
+		that = this;
+		
+		that.highAccuracy = !!enableHighAccuracy;
+		updateLocation();
+	}
+	com.magadanski.mSimpleNav.GPS = GPS;
 	
-	function setSensorListeners() {
+	// private properties
+	var location,
+		timeout = 2000;
+	
+	// public properties
+	GPS.prototype.highAccuracy = true;
+	
+	// private methods
+	function updateLocation() {
 		if (navigator.geolocation) {
-			currentLocationTimeout = navigator.geolocation.watchPosition(function (position) {
-				setCurrentLocation(position.coords.latitude, position.coords.longitude);
+			navigator.geolocation.getCurrentPosition(function (position) {
+				location = new LatLon(position.coords.latitude, position.coords.longitude);
+				
+				// TODO: add cutom event dispatch capabilities
+				// that.dispatchEvent(new CustomEvent('locationChange', {
+				// 	detail: {
+				// 		message: 'device location has changed',
+				// 		lat: lat,
+				// 		lng: lng
+				// 	},
+				// 	bubbles: true,
+				// 	cancelable: true
+				// }));
+				
+				setTimeout(function () {
+					// make new request no sooner than timeout AND next frame render
+					requestAnimationFrame(updateLocation);
+				}, timeout);
 			}, function () {
-				// do nothing
-			}, { enableHighAccuracy: true });
+				setTimeout(function () {
+					// make new request no sooner than timeout AND next frame render
+					requestAnimationFrame(updateLocation);
+				}, timeout);
+			}, { enableHighAccuracy: !!that.highAccuracy });
 		}
+	}
+	
+	// public methods
+	GPS.prototype.getDistanceTo = function (destination) {
+		var distance = '';
+		
+		if (location instanceof LatLon && destination instanceof LatLon) {
+			distance = location.distanceTo(destination);
+		}
+		
+		return distance;
+	}
+	
+	GPS.prototype.getBearingTo = function (destination) {
+		var bearing = 0;
+		
+		if (location instanceof LatLon && destination instanceof LatLon) {
+			bearing = location.bearingTo(destination);
+		}
+		
+		return bearing;
+	}
+})();
+
+//////////////////////////////////////
+// Compass Class Implementation //////
+//////////////////////////////////////
+(function () {
+	// import Class
+	var that;
+	var Compass = function () {
+		that = this;
 		
 		if (window.DeviceOrientationEvent) {
 			window.addEventListener('deviceorientation', function (e) {
@@ -28,142 +98,171 @@ function mSimpleNav() {
 					compassHeading = e.webkitCompassHeading;
 				}
 				
-				if (typeof(compassHeading)) {
-					setDeviceBearing(compassHeading);
-				}
+				tilt = e[tiltProperty] * tiltQuantifier;
 				
-				setDeviceTilt(e[tiltProperty] * tiltQuantifier);
-			}, false);
+				// TODO: add cutom event dispatch capabilities
+				// that.dispatchEvent(new CustomEvent('compassupdate', {
+				// 	detail: {
+				// 		message: 'compass data has changed',
+				// 		deviceBearing: compassHeading,
+				// 		deviceTilt: tilt,
+				// 	},
+				// 	bubbles: true,
+				// 	cancelable: true
+				// }));
+			});
 		}
 		
 		window.addEventListener('orientationchange', function (e) {
 			if (window.orientation == 90 || window.orientation == -90) {
 				tiltProperty = 'gamma';
-				if (window.orientation == 90) {
-					tiltQuantifier = -1;
-				} else {
-					tiltQuantifier = 1;
-				}
 			} else {
 				tiltProperty = 'beta';
+			}
+			
+			if (window.orientation > 0) {
+				tiltQuantifier = -1;
+			} else {
 				tiltQuantifier = 1;
 			}
 		});
 	}
+	com.magadanski.mSimpleNav.Compass = Compass;
 	
-	function removeSensorListeners() {
-		navigator.geolocation.clearWatch(currentLocationTimeout);
-		navigator.compass.clearWatch(deviceBearingTimeout);
+	// private properties
+	var compassHeading = 0,
+		tilt = 0,
+		tiltProperty = 'beta',
+		tiltQuantifier = 1;
+	
+	// public properties
+	
+	// private methods
+	
+	// public methods
+	Compass.prototype.getDeviceBearing = function () {
+		return compassHeading;
 	}
 	
-	function setCurrentLocation(lat, lng) {
-		currentLocation = new LatLon(lat, lng);
-		render();
+	Compass.prototype.getDeviceTilt = function () {
+		return tilt;
 	}
-	
-	function setDeviceBearing(bearing) {
-		deviceBearing = bearing;
-		render();
-	}
-	
-	function setDeviceTilt(tilt) {
-		deviceTilt = tilt;
-		render();
-	}
-	
-	function setDestinationBearing() {
-		if (currentLocation instanceof LatLon && destination instanceof LatLon) {
-			destinationBearing = currentLocation.bearingTo(destination);
-			render();
-		}
-	}
-	
-	function getCompassBearing() {
-		var newCompassBearing = destinationBearing - deviceBearing;
+})();
+
+//////////////////////////////////////
+// MSimpleNav Class Implementation ///
+//////////////////////////////////////
+(function () {
+	// import class
+	var that;
+	var MSimpleNav = function () {
+		that = this;
 		
-		if ( Math.abs( compassBearing - newCompassBearing ) < Math.abs( 360 + compassBearing - newCompassBearing ) ) {
-			compassBearing = newCompassBearing;
-		} else {
-			compassBearing = newCompassBearing - 360;
-		}
+		geocoder = new google.maps.Geocoder();
+		gps = new com.magadanski.mSimpleNav.GPS(true);
+		compass = new com.magadanski.mSimpleNav.Compass();
 		
-		return compassBearing;
+		render();
 	}
+	com.magadanski.mSimpleNav.MSimpleNav = MSimpleNav;
 	
+	// private properties
+	var destination,
+		geocoder,
+		gps,
+		compass,
+		displayBearing;
+	
+	// public properties
+	
+	// private methods
 	function render() {
-		if (currentLocation instanceof LatLon && destination instanceof LatLon) {
-			document.getElementById('distance').innerText = currentLocation.distanceTo(destination) + ' km';
-			document.getElementById('compass').style.webkitTransform = 'rotateX(' + deviceTilt + 'deg) rotateZ(' + getCompassBearing() + 'deg)';
+		var newBearing = gps.getBearingTo(destination) - compass.getDeviceBearing();
+		
+		if ( Math.abs( displayBearing - newBearing ) < Math.abs( 360 + displayBearing - newBearing ) ) {
+			displayBearing = newBearing;
+		} else {
+			displayBearing = newBearing - 360;
 		}
+		
+		document.getElementById('distance').innerText = gps.getDistanceTo(destination) + ' km';
+		document.getElementById('compass').style.webkitTransform = 'rotateX(' + compass.getDeviceTilt() + 'deg) rotateZ(' + displayBearing + 'deg)';
+		
+		requestAnimationFrame(render);
 	}
 	
-	mSimpleNav.prototype.setDestination = function (lat, lng) {
+	// public methods
+	MSimpleNav.prototype.setDestination = function (lat, lng) {
+		// normalize lat and lng values
+		while (lat > 90) { lat -= 180; }
+		while (lat < -90) { lat += 180; }
+		while (lng > 180) { lng -= 360; }
+		while (lng < -180) { lat += 360; }
+		
 		destination = new LatLon(lat, lng);
-		setDestinationBearing();
+		
+		// TODO: add cutom event dispatch capabilities
+		// that.dispatchEvent(new CustomEvent('destinationChange', {
+		// 	detail: {
+		// 		message: 'destination has changed',
+		// 		lat: lat,
+		// 		lng: lng
+		// 	},
+		// 	bubbles: true,
+		// 	cancelable: true
+		// }));
 	}
 	
-	setSensorListeners();
-	document.addEventListener('pause', removeSensorListeners, false);
-	document.addEventListener('resume', setSensorListeners, false);
-}
+	MSimpleNav.prototype.geocode = function (address) {
+		// TODO: i18n
+		geocoder.geocode({ address: address }, function (result, status) {
+			switch (status) {
+				case google.maps.GeocoderStatus.OK:
+					// Bookmark address capabilities
+					var location = result[0].geometry.location;
+					
+					that.setDestination(location.lat(), location.lng());
+					break;
+				case google.maps.GeocoderStatus.ERROR:
+					alert('Cannot get coordinates for location. Check your internet connection.');
+					break;
+				case google.maps.GeocoderStatus.INVALID_REQUEST:
+					alert('Contact the developer and let them know that an invalid request error occured.');
+					break;
+				case google.maps.GeocoderStatus.OVER_QUERY_LIMIT:
+					alert('You have made too many queries for too short time. Try again in 10 minutes.');
+					break;
+				case google.maps.GeocoderStatus.REQUEST_DENIED:
+					alert('Contact the developer and let them know that a request denied error occured.');
+					break;
+				case google.maps.GeocoderStatus.UNKNOWN_ERROR:
+					alert('Ooops, we could not find what you were looking for. Please try again.');
+					break;
+				case google.maps.GeocoderStatus.ZERO_RESULTS:
+					alert('No results were found for that address. Please try modifying that and searching again.');
+					break;
+			}
+		});
+	}
+})();
 
 document.addEventListener('DOMContentLoaded', function (e) {
+	var app = new com.magadanski.mSimpleNav.MSimpleNav();
+	var addressForm = document.getElementById('address-form');
+	var coordinatesForm = document.getElementById('coordinates-form');
+	
+	coordinatesForm.addEventListener('submit', function (e) {
+		e.preventDefault();
+		app.setDestination(document.getElementById('lat').value, document.getElementById('lng').value);
+	});
+	
+	addressForm.addEventListener('submit', function (e) {
+		e.preventDefault();
+		app.geocode(document.getElementById('address').value);
+	});
+	
 	// stupid iOS7
 	if (navigator.userAgent.match(/CPU\sOS\s7_/)) {
 		document.body.style.paddingTop = '24px';
-	}
-	
-	var geocoder;
-	
-	simpleNav = new mSimpleNav();
-	
-	document.getElementById('address-form').addEventListener('submit', function (e) {
-		e.preventDefault();
-		
-		if (typeof(google) != 'undefined' && typeof(google.maps) != 'undefined' && typeof(google.maps.Geocoder) != 'undefined') {
-			geocoder = new google.maps.Geocoder();
-			
-			geocoder.geocode({ address: document.getElementById('address').value }, function (result, status) {
-				switch (status) {
-					case google.maps.GeocoderStatus.OK:
-						var location = result[0].geometry.location;
-						
-						document.getElementById('lat').value = location.lat();
-						document.getElementById('lng').value = location.lng();
-						
-						searchByCoordinates();
-						break;
-					case google.maps.GeocoderStatus.ERROR:
-						alert('Cannot get coordinates for location. Check your internet connection.');
-						break;
-					case google.maps.GeocoderStatus.INVALID_REQUEST:
-						alert('Contact the developer and let them know that an invalid request error occured.');
-						break;
-					case google.maps.GeocoderStatus.OVER_QUERY_LIMIT:
-						alert('You have made too many queries for too short time. Try again in 10 minutes.');
-						break;
-					case google.maps.GeocoderStatus.REQUEST_DENIED:
-						alert('Contact the developer and let them know that a request denied error occured.');
-						break;
-					case google.maps.GeocoderStatus.UNKNOWN_ERROR:
-						alert('Ooops, we could not find what you were looking for. Please try again.');
-						break;
-					case google.maps.GeocoderStatus.ZERO_RESULTS:
-						alert('No results were found for that address. Please try modifying that and searching again.');
-						break;
-				}
-			});
-		} else {
-			alert('You cannot currently search by address. Try a direct GPS coordinates search');
-		}
-	});
-	
-	document.getElementById('coordinates-form').addEventListener('submit', function (e) {
-		e.preventDefault();
-		searchByCoordinates();
-	});
-	
-	function searchByCoordinates() {
-		simpleNav.setDestination(document.getElementById('lat').value, document.getElementById('lng').value);
 	}
 });
