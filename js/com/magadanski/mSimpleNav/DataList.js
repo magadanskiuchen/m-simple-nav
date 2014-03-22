2703180e1463250eb5ab463201936b711bce4045
Georgi Popov <magadanski.uchen@gmail.com>
2014-03-22 15:08:49 +0200

    added empty DataList class file

---
+++ js/com/magadanski/mSimpleNav/DataList.js
@@ -0,0 +1,23 @@
+com.magadanski.mSimpleNav.DataList;
+
+//////////////////////////////////////
+// DataList Class Implementation /////
+//////////////////////////////////////
+(function () {
+	// import Class
+	var that;
+	var DataList = function () {
+		that = this;
+	}
+	DataList.inherits(com.magadanski.EventDispatcher);
+	com.magadanski.mSimpleNav.DataList = DataList;
+	
+	// private properties
+	
+	// public properties
+	
+	// private methods
+	
+	// public methods
+	
+})();
