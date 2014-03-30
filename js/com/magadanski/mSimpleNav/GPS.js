9bbd5187cfba81c656ecd5e021936ab19d9b88ab
Georgi Popov <magadanski.uchen@gmail.com>
2014-03-30 19:48:07 +0300

    GPS class access fixes

--- js/com/magadanski/mSimpleNav/GPS.js
+++ js/com/magadanski/mSimpleNav/GPS.js
@@ -5,15 +5,32 @@ com.magadanski.mSimpleNav.GPS;
 //////////////////////////////////////
 (function () {
 	// import class
-	var that;
 	var GPS = function (enableHighAccuracy) {
-		that = this;
+		var that = this;
 		
 		if (typeof(enableHighAccuracy) == 'undefined') {
 			enableHighAccuracy = true;
 		}
 		
 		that.highAccuracy = !!enableHighAccuracy;
+		
+		function updateLocation() {
+			if (navigator.geolocation) {
+				navigator.geolocation.getCurrentPosition(function (position) {
+					that.location = new LatLon(position.coords.latitude, position.coords.longitude);
+					
+					that.dispatchEvent('locationChange', {
+						message: 'device location has changed',
+						lat: that.location.lat(),
+						lng: that.location.lon()
+					});
+				}, function () {
+					that.dispatchEvent('locationFault', {
+						message: 'no information on device location'
+					});
+				}, { enableHighAccuracy: !!that.highAccuracy });
+			}
+		}
 		
 		that.addEventListener('locationChange, locationFault', function (e) {
 			setTimeout(function () {
@@ -28,47 +45,32 @@ com.magadanski.mSimpleNav.GPS;
 	com.magadanski.mSimpleNav.GPS = GPS;
 	
 	// private properties
-	var location,
-		timeout = 2000;
+	var timeout = 2000;
 	
 	// public properties
+	GPS.prototype.location = null;
 	GPS.prototype.highAccuracy = true;
 	
 	// private methods
-	function updateLocation() {
-		if (navigator.geolocation) {
-			navigator.geolocation.getCurrentPosition(function (position) {
-				location = new LatLon(position.coords.latitude, position.coords.longitude);
-				
-				that.dispatchEvent('locationChange', {
-					message: 'device location has changed',
-					lat: location.lat(),
-					lng: location.lon()
-				});
-			}, function () {
-				that.dispatchEvent('locationFault', {
-					message: 'no information on device location'
-				});
-			}, { enableHighAccuracy: !!that.highAccuracy });
-		}
-	}
 	
 	// public methods
 	GPS.prototype.getDistanceTo = function (destination) {
+		var that = this;
 		var distance = '';
 		
-		if (location instanceof LatLon && destination instanceof LatLon) {
-			distance = location.distanceTo(destination);
+		if (that.location instanceof LatLon && destination instanceof LatLon) {
+			distance = that.location.distanceTo(destination);
 		}
 		
 		return distance;
 	}
 	
 	GPS.prototype.getBearingTo = function (destination) {
+		var that = this;
 		var bearing = 0;
 		
-		if (location instanceof LatLon && destination instanceof LatLon) {
-			bearing = location.bearingTo(destination);
+		if (that.location instanceof LatLon && destination instanceof LatLon) {
+			bearing = that.location.bearingTo(destination);
 		}
 		
 		return bearing;
