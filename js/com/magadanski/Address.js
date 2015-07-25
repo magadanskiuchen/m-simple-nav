89a1d6a1552df82f76a80c23937e6ececfe775ee
Georgi Popov <magadanski.uchen@gmail.com>
2015-07-25 19:59:31 +0300

    UI optimizations

---
+++ js/com/magadanski/Address.js
@@ -0,0 +1,51 @@
+com.magadanski.Address;
+
+//////////////////////////////////////
+// Address Class Implementation //////
+//////////////////////////////////////
+(function () {
+	// import class
+	var that;
+	var Address = function () {
+		that = this;
+		
+		// private properties
+		var hash = '';
+		var state = '';
+		
+		// private methods
+		function popStateHandler(e) {
+			hash = window.location.hash;
+			state = e.state;
+		}
+		
+		// priviledged properties
+		
+		// priviledged methods
+		that.getHash = function () {
+			return hash;
+		}
+		
+		that.setHash = function (hash, title, state) {
+			var old = { hash: that.getHash(), state: that.getState() };
+			
+			if (old.hash != hash) {
+				history.pushState(title, state, hash);
+				
+				window.dispatchEvent(new PopStateEvent('popstate', { state: state }));
+				that.dispatchEvent('change', { previousHash: old.hash, previousState: old.state });
+			}
+		}
+		
+		that.getState = function () {
+			return state;
+		}
+		
+		// constructor
+		window.addEventListener('popstate', popStateHandler);
+	}
+	Address.inherits(com.magadanski.EventDispatcher);
+	com.magadanski.Address = Address;
+	
+	// public methods
+})();
